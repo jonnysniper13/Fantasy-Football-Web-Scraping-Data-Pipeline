9cc4a9387945381dbbc531f1b78c7380b90d8026
Jonny <j.holmes.eng@gmail.com>
2022-02-21 21:46:50 +0000

    Checking

--- project/fpl_webscraper.py
+++ project/fpl_webscraper.py
@@ -14,7 +14,6 @@ The only usage of this module is to initiate an instance of the Class:
 scraped_data = WebScraper()
 """
 
-
 from pydantic import NonNegativeFloat
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
